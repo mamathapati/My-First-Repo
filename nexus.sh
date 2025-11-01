1bb287d4c3e468da7c1b0cd145f856091ecf8f62
mamathapati <mamathapati9@gmail.com>
2025-11-02 02:10:03 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,17 +1,23 @@
 sudo yum update -y
 sudo yum install wget -y
-sudo yum install java-17-amazon-corretto-jmods -y
-sudo mkdir /app && cd /app
+sudo yum install java-17-amazon-corretto -y
+
+sudo mkdir -p /app && cd /app
 sudo wget https://download.sonatype.com/nexus/3/nexus-3.85.0-03-linux-x86_64.tar.gz
 sudo tar -xvf nexus-3.85.0-03-linux-x86_64.tar.gz
 sudo mv nexus-3.85.0-03 nexus
+
 sudo adduser nexus
-sudo chown -R nexus:nexus /app/nexus
-sudo chown -R nexus:nexus /app/sonatype*
-sudo sed -i '27  run_as_user="nexus"' /app/nexus/bin/nexus
+sudo mkdir -p /app/sonatype-work
+sudo chown -R nexus:nexus /app/nexus /app/sonatype-work
+
+# Set nexus user
+sudo sed -i 's|^#run_as_user=.*|run_as_user="nexus"|' /app/nexus/bin/nexus
+
+# Create systemd service
 sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
 [Unit]
-Description=nexus service
+Description=Nexus Repository Manager
 After=network.target
 
 [Service]
@@ -21,13 +27,14 @@ User=nexus
 Group=nexus
 ExecStart=/app/nexus/bin/nexus start
 ExecStop=/app/nexus/bin/nexus stop
-User=nexus
 Restart=on-abort
 
 [Install]
 WantedBy=multi-user.target
 EOL
-sudo chkconfig nexus on
-sudo systemctl start nexus
+
+# Enable and start Nexus
+sudo systemctl daemon-reload
 sudo systemctl enable nexus
+sudo systemctl start nexus
 sudo systemctl status nexus
